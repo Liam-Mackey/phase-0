22be09cda60c5f092ce7605e06906fed636b6da1
Liam Mackey <liammac21@gmail.com>
2015-11-30 16:35:07 -0500

    Added 4.6.5

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,21 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  ans = []
+  array_1.each do |x|
+  	ans.push(x)
+  end 
+
+  array_2.each do |x|
+  	ans.push(x)
+  end
+
+  return ans
+end
+
+# the easy solution is to just use the concat method for arrays
